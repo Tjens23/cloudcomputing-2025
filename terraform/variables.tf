7332311f003159cdccbf0661afe561a6f0b20285
zenrough <12938899-Zenrough@users.noreply.gitlab.com>
2025-11-28 10:34:40 +0100

    added the changed files finally

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable region {
+    type    = string
+    default = "europe-north2"
+}
+
+variable project {
+    type    = string
+    default = "cloudcomputing-terraform"
+}
+
+variable frontend_image {
+    type    = string
+    default = "gcr.io/cloudcomputing-terraform/frontend:latest"
+}
+
+variable backend_image {
+    type    = string
+    default = "gcr.io/cloudcomputing-terraform/backend:latest"
+}
+
+variable sql_version {
+    type    = string
+    default = "MYSQL_8_0"
+}

e2bda2828059ce8bdaa6a8a0502fb20140eed784
zenrough <12938899-Zenrough@users.noreply.gitlab.com>
2025-11-29 13:07:22 +0100

    added vpc peering unsure if works

--- terraform/network.tf
+++ terraform/network.tf
@@ -15,7 +15,6 @@ name = "backend-frontend-name"
 auto_create_subnetworks = false
 }
 
-# todo change subnet ranges to being different
 resource "google_compute_subnetwork""backend-subnet" {
 name = "backend-subnet"
 ip_cidr_range = "10.0.0.0/24"
@@ -37,6 +36,8 @@ region = var.region
 network =google_compute_network.backend.id
 }
 
+# TODO connector
+
 # resource "google_vpc_access_connector" "connector" {
 #   name          = "vpc-con"
 #   ip_cidr_range = "10.8.0.0/28"
@@ -44,3 +45,47 @@ network =google_compute_network.backend.id
 #   min_instances = 2
 #   max_instances = 3
 # }
+
+# add firewall rule to backend allowing connection in from other vpc eg. frontend
+resource "google_compute_firewall" "backend-firewall" {
+  project = var.project
+  name    = "backend-to-frontend"
+  network = google_compute_network.backend.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  # which ranges to open up for right?
+  source_ranges = ["10.0.1.0/24"]
+}
+
+# add firewall rule to frontend allowing connection from the public
+resource "google_compute_firewall" "frontend-firewall" {
+  project = var.project
+  name    = "frontend-backend"
+  network = google_compute_network.frontend.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  # which ranges to open up for right?
+  source_ranges = ["10.0.0.0/24"]
+}
+
+# add vpc peering #
+
+# frontend to backend vpc
+resource "google_compute_network_peering" "frontend-to-backend" {
+  name         = "backendpeerer"
+  network      = google_compute_network.frontend.id
+  peer_network = google_compute_network.backend.id
+}
+
+# frontend to backend vpc
+resource "google_compute_network_peering" "backend-to-frontend" {
+  name         = "frontendpeerer"
+  network      = google_compute_network.frontend.id
+  peer_network = google_compute_network.backend.id
+}

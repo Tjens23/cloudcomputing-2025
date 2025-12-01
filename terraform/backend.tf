67fac9d31f6fc5fcc5628709b2ea4cf16540f25a
tjens23 <tjens23@student.sdu.dk>
2025-12-01 16:29:52 +0100

    Update IAM role and member for Cloud Run VPC access

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 resource "google_project_iam_member" "cloud_run_vpc_access_agent" {
   project = var.project
-  role    = "roles/run.vpcAccessUser"
-  member  = "serviceAccount:service-1028655243443@gcp-sa-cloudrun.iam.gserviceaccount.com"
+  role    = "roles/compute.networkUser"
+  member  = "serviceAccount:service-${var.project_number}@serverless-robot-prod.iam.gserviceaccount.com"
 }
 
 resource "google_cloud_run_v2_service" "backend_service" {

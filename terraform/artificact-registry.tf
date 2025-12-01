d6bc6a5756bd27307eeb09f0e0274eaea6a7b224
Mathias <mathias.yde@hotmail.dk>
2025-12-01 18:03:35 +0100

    Remove redudant code

--- terraform/artificact-registry.tf
+++ terraform/artificact-registry.tf
@@ -5,20 +5,9 @@ resource "google_artifact_registry_repository" "backend" {
   format        = "DOCKER"
 }
 
-# original project has a registry for each image
-# import {
-#   id = "projects/cloudcomputing-471208/locations/europe-north1/repositories/backend-repo"
-#   to = google_artifact_registry_repository.backend
-# }
-
 resource "google_artifact_registry_repository" "frontend" {
   location      = var.region
   repository_id = "frontend-repo"
   description   = "example docker repository"
   format        = "DOCKER"
 }
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/artifact_registry_repository#import
-# import {
-#   id = "projects/cloudcomputing-471208/locations/europe-north1/repositories/cloud-run-source-deploy"
-#   to = google_artifact_registry_repository.frontend
-# }

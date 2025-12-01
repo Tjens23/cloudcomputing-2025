d441374c2c6bcb7840f0e09b1e2a7c7fe186928e
Mathias <mathias.yde@hotmail.dk>
2025-12-01 13:35:33 +0100

    fix iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,17 +1,11 @@
-resource "google_project_iam_policy" "iam_policy" {
-  project     = var.project
-  policy_data = data.google_iam_policy.admin.policy_data
-}
+resource "google_project_iam_binding" "editors" {
+  project = var.project
+  role    = "roles/editor"
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/editor"
-
-    members = [
-      "user:mathiasydesdu@gmail.com",
-      "user:hopetobylol@gmail.com",
-      "user:Mathias.rojleskov@gmail.com",
-      "user:Phillip713dk@gmail.com"
-    ]
-  }
+  members = [
+    "user:mathiasydesdu@gmail.com",
+    "user:hopetobylol@gmail.com",
+    "user:Mathias.rojleskov@gmail.com",
+    "user:Phillip713dk@gmail.com",
+  ]
 }
